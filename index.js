611b9011d4cd6893a6c3df4e903ebcd1035b0814
Rian Rahman <rian.rahman@gatech.edu>
2023-05-17 17:44:19 -0400

    Added foreground

--- index.js
+++ index.js
@@ -11,21 +11,6 @@ for (let i = 0; i < collisions.length; i += 70) {
     collisionsMap.push(collisions.slice(i, i + 70))
 }
 
-class Boundary {
-    static width = 48
-    static height = 48
-    constructor({position}) {
-        this.position = position
-        this.width = 40
-        this.height = 40
-    }
-
-    draw() {
-        context.fillStyle = 'rgba(255, 0, 0, 0.0)'
-        context.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-}
-
 const boundaries = []
 const offset = {
     x: -785,
@@ -53,6 +38,9 @@ canvas.fillStyle = 'white'
 const image = new Image()
 image.src = './img/pelletTown.png'
 
+const foregroundImage = new Image()
+foregroundImage.src = './img/foreground.png'
+
 const playerImage = new Image()
 playerImage.src = './img/playerDown.png'
 
@@ -69,32 +57,6 @@ const keys = {
     d: {
         pressed: false
     },
-}
-
-
-class Sprite {
-    constructor({position, velocity, image, frames = {max: 1}}) {
-        this.position = position
-        this.image = image
-        this.frames = frames
-        this.image.onload = () => {
-            this.width = this.image.width / this.frames.max
-            this.height = this.image.height
-        }
-    }
-    draw() {
-        context.drawImage(
-            this.image,
-            0,
-            0,
-            this.image.width / this.frames.max,
-            this.image.height, 
-            this.position.x,
-            this.position.y,
-            this.image.width / this.frames.max,
-            this.image.height
-        )
-    }
 }
 
 // canvas.width / 2 - (this.image.width / 4) / 2, 
@@ -118,7 +80,16 @@ const background = new Sprite({
     },
     image: image
 })
-const movables = [background, ...boundaries]
+
+const foreground = new Sprite({
+  position: {
+      x: offset.x + 432,
+      y: offset.y + 145
+  },
+  image: foregroundImage
+})
+
+const movables = [background, ...boundaries, foreground]
 
 function rectangularCollision({ rectangle1, rectangle2 }) {
     return (
@@ -130,7 +101,7 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
   }
   
 
-let speed = 12
+let speed = 3
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -140,6 +111,7 @@ function animate() {
     })
 
     player.draw()
+    foreground.draw()
 
     let moving = true
     if (keys.w.pressed && lastKey === 'w') {
