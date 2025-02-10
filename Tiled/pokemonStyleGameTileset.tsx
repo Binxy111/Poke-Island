bd4caa3d28de08d6c79aafece3a68116ec4480d2
Binxy111 <xxbinxcatxx@gmail.com>
2025-02-10 17:39:51 -0500

    Update pokemonStyleGameTileset.tsx

--- Tiled/pokemonStyleGameTileset.tsx
+++ Tiled/pokemonStyleGameTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="sceneSpriteSheet" tilewidth="12" tileheight="12" tilecount="1024" columns="32">
- <image source="tileset.png" width="384" height="384"/>
+<tileset version="1.5" tiledversion="1.7.2" name="sceneSpriteSheet" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c6ff6d3a-ea05-4d64-8a86-08a4f5f352ac/dg7ast6-54a25feb-6c53-4db4-97bd-97f1ad48426a.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2M2ZmY2ZDNhLWVhMDUtNGQ2NC04YTg2LTA4YTRmNWYzNTJhY1wvZGc3YXN0Ni01NGEyNWZlYi02YzUzLTRkYjQtOTdiZC05N2YxYWQ0ODQyNmEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.aJPFNVhab6sUPsr2_92KZH1sb-iCva_894kPkAViDkU" width="384" height="384"/>
 </tileset>
